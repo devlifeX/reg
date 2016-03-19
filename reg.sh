881666c786f027508eb704520b88dbce996e642f
Dariush vesal <dariush.vesal@gmail.com>
2016-03-19 15:24:40 +0330

    Modify path

--- reg.sh
+++ reg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
   REG_CURRENT_PATH=$(pwd);
 
-. $REG_CURRENT_PATH/inc/reg_var.sh
-. $REG_CURRENT_PATH/inc/reg_common.sh
-. $REG_CURRENT_PATH/inc/reg_command_selector.sh
-. $REG_CURRENT_PATH/inc/reg_man.sh
-. $REG_CURRENT_PATH/inc/reg_load.sh
-. $REG_CURRENT_PATH/inc/reg_save.sh
-. $REG_CURRENT_PATH/inc/reg_remove.sh
-. $REG_CURRENT_PATH/inc/reg_exe.sh
+. "$REG_CURRENT_PATH/inc/reg_var.sh"
+. "$REG_CURRENT_PATH/inc/reg_common.sh"
+. "$REG_CURRENT_PATH/inc/reg_command_selector.sh"
+. "$REG_CURRENT_PATH/inc/reg_man.sh"
+. "$REG_CURRENT_PATH/inc/reg_load.sh"
+. "$REG_CURRENT_PATH/inc/reg_save.sh"
+. "$REG_CURRENT_PATH/inc/reg_remove.sh"
+. "$REG_CURRENT_PATH/inc/reg_exe.sh"
 
 reg () {
   reg_command "$@";
